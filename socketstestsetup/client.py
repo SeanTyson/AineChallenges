7514d00a0aafceae799adb0ec4f0f36ec05ae447
Sean Tyson <codeypony@gmail.com>
2024-12-09 20:46:06 +0000

    test sockets

---
+++ socketstestsetup/client.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+
+HOST = "localhost"
+PORT = 5050
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
+            print(f"Message from server: {message}\nEnter a message: \n")
+        except:
+            print("Connection closed by the server.")
+            break
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+    print("Connected to the server!")
+
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread.start()
+
+    try:
+        while True:
+            message = input("Enter a message: ")
+            client_socket.sendall(message.encode('utf-8'))
+    except KeyboardInterrupt:
+        print("Disconnecting...")
+    finally:
+        client_socket.close()
+
+if __name__ == "__main__":
+    main()
---
+++ socketstestsetup/server.py
@@ -0,0 +1,65 @@
+import socket
+import threading
+
+
+HOST = "0.0.0.0" 
+PORT = 5050
+
+
+clients = []  s
+messages = []  
+messages_lock = threading.Lock()
+
+def broadcast(message, sender_socket):
+    for client in clients:
+        if client != sender_socket: 
+            try:
+                client.sendall(message.encode('utf-8'))
+            except:
+                clients.remove(client)  
+
+def handle_client(client_socket):
+    while True:
+        try:
+          
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break  
+
+            with messages_lock:
+                messages.append(message)
+
+            broadcast(message, client_socket)
+        except:
+            break
+
+    clients.remove(client_socket)
+    client_socket.close()
+    print("Client disconnected")
+
+def start_server():
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(2) 
+        print(f"Server started on {HOST}:{PORT}")
+
+        while True:
+            client_socket, client_address = server_socket.accept()
+            print(f"New connection from {client_address}")
+            clients.append(client_socket)
+
+            client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+            client_thread.start()
+    except KeyboardInterrupt:
+        print("\nShutting down the server...")
+    finally:
+        for client in clients:
+            client.close()
+        if server_socket:
+            server_socket.close()
+        print("Server closed.")
+
+
+if __name__ == "__main__":
+    start_server()
